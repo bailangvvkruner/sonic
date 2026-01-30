c82aa486bfddf0ada2c5df1b369c397e32a65763
bailangvvkg <bailangvvking@outlook.com>
2026-01-30 15:41:57 +0800

    post cache

--- handler/content/model/init.go
+++ handler/content/model/init.go
@@ -2,9 +2,17 @@ package model
 
 import (
 	"github.com/go-sonic/sonic/injection"
+	"github.com/go-sonic/sonic/service/impl"
 )
 
 func init() {
+	// 注册缓存服务
+	injection.Provide(impl.NewCachedPostService)
+	injection.Provide(impl.NewCachedCategoryService)
+	injection.Provide(impl.NewCachedTagService)
+	// 使用带缓存的 PostModel 替换原有的 PostModel
+	// 注意：这里 CachedPostModel 和 PostModel 有相同的方法集
+	// 但 fx 不支持直接替换类型，需要在 handler 中修改依赖
 	injection.Provide(NewPostModel)
 	injection.Provide(NewCategoryModel)
 	injection.Provide(NewSheetModel)
--- handler/content/model/post.go
+++ handler/content/model/post.go
@@ -10,8 +10,10 @@ import (
 	"github.com/go-sonic/sonic/model/entity"
 	"github.com/go-sonic/sonic/model/param"
 	"github.com/go-sonic/sonic/model/property"
+	"github.com/go-sonic/sonic/model/vo"
 	"github.com/go-sonic/sonic/service"
 	"github.com/go-sonic/sonic/service/assembler"
+	"github.com/go-sonic/sonic/service/impl"
 	"github.com/go-sonic/sonic/template"
 	"github.com/go-sonic/sonic/util/xerr"
 )
@@ -26,6 +28,7 @@ func NewPostModel(optionService service.OptionService,
 	postAssembler assembler.PostAssembler,
 	metaService service.MetaService,
 	postAuthentication *authentication.PostAuthentication,
+	cachedPostService *impl.CachedPostService,
 ) *PostModel {
 	return &PostModel{
 		OptionService:       optionService,
@@ -38,6 +41,7 @@ func NewPostModel(optionService service.OptionService,
 		TagService:          tagService,
 		MetaService:         metaService,
 		PostAuthentication:  postAuthentication,
+		CachedPostService:   cachedPostService,
 	}
 }
 
@@ -52,6 +56,7 @@ type PostModel struct {
 	MetaService         service.MetaService
 	PostAssembler       assembler.PostAssembler
 	PostAuthentication  *authentication.PostAuthentication
+	CachedPostService   *impl.CachedPostService
 }
 
 func (p *PostModel) Content(ctx context.Context, post *entity.Post, token string, model template.Model) (string, error) {
@@ -71,48 +76,41 @@ func (p *PostModel) Content(ctx context.Context, post *entity.Post, token string
 		}
 	}
 
-	postVO, err := p.PostAssembler.ConvertToDetailVO(ctx, post)
+	// 使用缓存获取文章详情
+	postVO, err := p.CachedPostService.GetPostDetailVO(ctx, post)
 	if err != nil {
 		return "", err
 	}
 	model["post"] = postVO
 
-	prevPosts, err := p.PostService.GetPrevPosts(ctx, post, 1)
+	// 使用缓存获取上下篇文章
+	prevPost, nextPost, err := p.CachedPostService.GetPrevNextPosts(ctx, post)
 	if err != nil {
 		return "", err
 	}
-	nextPosts, err := p.PostService.GetNextPosts(ctx, post, 1)
-	if err != nil {
-		return "", err
+	if prevPost != nil {
+		model["prevPost"] = prevPost
 	}
-	if len(prevPosts) > 0 {
-		prePost, err := p.PostAssembler.ConvertToDetailVO(ctx, prevPosts[0])
-		if err != nil {
-			return "", err
-		}
-		model["prevPost"] = prePost
-	}
-	if len(nextPosts) > 0 {
-		nextPost, err := p.PostAssembler.ConvertToDetailVO(ctx, nextPosts[0])
-		if err != nil {
-			return "", err
-		}
+	if nextPost != nil {
 		model["nextPost"] = nextPost
 	}
 
-	categories, err := p.PostCategoryService.ListCategoryByPostID(ctx, post.ID)
+	// 使用缓存获取文章分类
+	categories, err := p.CachedPostService.GetPostCategories(ctx, post.ID)
 	if err != nil {
 		return "", err
 	}
 	model["categories"], _ = p.CategoryService.ConvertToCategoryDTOs(ctx, categories)
 
-	tags, err := p.PostTagService.ListTagByPostID(ctx, post.ID)
+	// 使用缓存获取文章标签
+	tags, err := p.CachedPostService.GetPostTags(ctx, post.ID)
 	if err != nil {
 		return "", err
 	}
 	model["tags"], _ = p.TagService.ConvertToDTOs(ctx, tags)
 
-	metas, err := p.MetaService.GetPostMeta(ctx, post.ID)
+	// 使用缓存获取文章 Meta
+	metas, err := p.CachedPostService.GetPostMeta(ctx, post.ID)
 	if err != nil {
 		return "", err
 	}
@@ -158,10 +156,23 @@ func (p *PostModel) List(ctx context.Context, page int, model template.Model) (s
 	if err != nil {
 		return "", err
 	}
-	postVOs, err := p.PostAssembler.ConvertToListVO(ctx, posts)
+
+	// 批量获取文章 ID
+	postIDs := make([]int32, len(posts))
+	for i, post := range posts {
+		postIDs[i] = post.ID
+	}
+
+	// 使用缓存批量获取标签和分类
+	tagMap, _ := p.CachedPostService.GetPostTagsMap(ctx, postIDs)
+	catMap, _ := p.CachedPostService.GetPostCategoriesMap(ctx, postIDs)
+
+	// 转换为 VO
+	postVOs, err := p.convertToListVOWithCache(ctx, posts, tagMap, catMap)
 	if err != nil {
 		return "", err
 	}
+
 	postPage := dto.NewPage(postVOs, totalCount, param.Pagination{
 		PageNum:  page,
 		PageSize: pageSize,
@@ -174,6 +185,48 @@ func (p *PostModel) List(ctx context.Context, page int, model template.Model) (s
 	model["meta_keywords"] = seoKeyWords
 	model["meta_description"] = seoDescription
 	return p.ThemeService.Render(ctx, "index")
+}
+
+// convertToListVOWithCache 使用缓存数据转换文章列表 VO
+func (p *PostModel) convertToListVOWithCache(
+	ctx context.Context,
+	posts []*entity.Post,
+	tagMap map[int32][]*entity.Tag,
+	catMap map[int32][]*entity.Category,
+) ([]*vo.Post, error) {
+	postVOs := make([]*vo.Post, 0, len(posts))
+
+	for _, post := range posts {
+		postVO := &vo.Post{}
+
+		// 从缓存获取分类
+		if categories, ok := catMap[post.ID]; ok {
+			categoryDTOs, err := p.CategoryService.ConvertToCategoryDTOs(ctx, categories)
+			if err != nil {
+				return nil, err
+			}
+			postVO.Categories = categoryDTOs
+		}
+
+		// 从缓存获取标签
+		if tags, ok := tagMap[post.ID]; ok {
+			tagDTOs, err := p.TagService.ConvertToDTOs(ctx, tags)
+			if err != nil {
+				return nil, err
+			}
+			postVO.Tags = tagDTOs
+		}
+
+		// 转换基础 DTO
+		postDTO, err := p.PostAssembler.ConvertToSimpleDTO(ctx, post)
+		if err != nil {
+			return nil, err
+		}
+		postVO.Post = *postDTO
+		postVOs = append(postVOs, postVO)
+	}
+
+	return postVOs, nil
 }
 
 func (p *PostModel) Archives(ctx context.Context, page int, model template.Model) (string, error) {
@@ -192,10 +245,20 @@ func (p *PostModel) Archives(ctx context.Context, page int, model template.Model
 	if err != nil {
 		return "", err
 	}
-	postVOs, err := p.PostAssembler.ConvertToListVO(ctx, posts)
+
+	// 使用缓存批量获取标签和分类
+	postIDs := make([]int32, len(posts))
+	for i, post := range posts {
+		postIDs[i] = post.ID
+	}
+	tagMap, _ := p.CachedPostService.GetPostTagsMap(ctx, postIDs)
+	catMap, _ := p.CachedPostService.GetPostCategoriesMap(ctx, postIDs)
+
+	postVOs, err := p.convertToListVOWithCache(ctx, posts, tagMap, catMap)
 	if err != nil {
 		return "", err
 	}
+
 	postPage := dto.NewPage(postVOs, totalPage, param.Pagination{
 		PageNum:  page,
 		PageSize: pageSize,
@@ -220,6 +283,7 @@ func (p *PostModel) AdminPreview(ctx context.Context, post *entity.Post, model t
 		return "", xerr.WithStatus(nil, int(xerr.StatusBadRequest)).WithMsg("查询不到文章信息")
 	}
 
+	// 预览不使用缓存，直接查询
 	postVO, err := p.PostAssembler.ConvertToDetailVO(ctx, post)
 	if err != nil {
 		return "", err
